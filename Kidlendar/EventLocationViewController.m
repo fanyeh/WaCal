776d07e7bac6d10b794cc0f4c32424824328861e
fanyeh <jack.ffy@gmail.com>
2014-02-16 23:07:41 +0800

    Changed google place to ios map kit

--- Kidlendar/EventLocationViewController.m
+++ Kidlendar/EventLocationViewController.m
@@ -9,21 +9,21 @@
 #import "EventLocationViewController.h"
 #import "EventLocationView.h"
 #import "CalendarStore.h"
-#import <GoogleMaps/GoogleMaps.h>
 #import "ImageStore.h"
+#import <MapKit/MapKit.h>
 
-#define kGOOGLE_API_KEY @"AIzaSyAD9e182Fr19_2DcJFZYUHf6wEeXjxs_kQ"
-#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+typedef void (^LocationCallback)(CLLocationCoordinate2D);
 
-@interface EventLocationViewController ()
+@interface EventLocationViewController () <MKMapViewDelegate>
 {
     CGRect titleViewFrameShow;
     EventLocationView *locationView;
     UIImageView *backgroundImageView;
-    GMSMapView *mapView_;
+    MKMapView *mapView_;
     CLGeocoder *geocoder;
     NSArray* places;
     UIActivityIndicatorView *activityIndicator;
+    LocationCallback _foundLocationCallback;
 }
 @end
 
@@ -42,7 +42,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    
+
     // Set up background
     backgroundImageView = [[UIImageView alloc]initWithFrame:self.view.frame];
     [self.view addSubview:backgroundImageView];
@@ -56,6 +56,7 @@
     locationView.locationField.text = _event.location;
     [locationView.locationField becomeFirstResponder];
     
+    // Table view to show searched locations
     locationView.searchedLocationTable.dataSource = self;
     locationView.searchedLocationTable.delegate = self;
     //[locationView.searchedLocation registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
@@ -66,9 +67,11 @@
     [self.view addSubview:activityIndicator];
     [activityIndicator stopAnimating];
     
-    mapView_ = [GMSMapView mapWithFrame:CGRectMake(10, 128, 300, 210) camera:nil];
-    mapView_.myLocationEnabled = YES;
+    mapView_ = [[MKMapView alloc]initWithFrame:CGRectMake(10, 128, 300, 210)];
+    mapView_.pitchEnabled = YES;
+    mapView_.showsUserLocation = YES;
     mapView_.hidden = YES;
+    mapView_.delegate = self;
     [self.view addSubview:mapView_];
     
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(clearText)];
@@ -81,41 +84,27 @@
     self.navigationItem.rightBarButtonItem = saveButton;
 }
 
--(void) queryGooglePlaces: (NSString *) name
+- (void)startSearchForText:(NSString*)searchText
 {
-    [locationView.searchedLocationTable setHidden:YES];
-    [activityIndicator startAnimating];
-    NSString *url = [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/place/textsearch/json?query=%@&sensor=true&language=zh-TW&key=%@",name,kGOOGLE_API_KEY];
-    //Formulate the string as a URL object.
-    NSURL *googleRequestURL=[NSURL URLWithString:[url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-    // Retrieve the results of the URL.
+    MKLocalSearchRequest *searchRequest = [[MKLocalSearchRequest alloc] init];
+    searchRequest.naturalLanguageQuery = searchText;
     
-    NSURLRequest *request = [NSURLRequest requestWithURL:googleRequestURL];
-    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-    [NSURLConnection sendAsynchronousRequest:request
-                                       queue:queue
-                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-                               
-                               if ([data length]>0 && connectionError==nil) {
-                                   //收到正確的資料，連線沒有錯
-                                   NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data
-                                                                                        options:NSJSONReadingAllowFragments
-                                                                                          error:&connectionError];
-                                   //The results from Google will be an array obtained from the NSDictionary object with the key "results".
-                                   places = [json objectForKey:@"results"];
-                                   dispatch_async(dispatch_get_main_queue(), ^{
-                                       [activityIndicator stopAnimating];
-                                       [locationView.searchedLocationTable setHidden:NO];
-                                       [locationView.searchedLocationTable reloadData];
-                                   });
-                                   
-                               } else if ([data length]==0 && connectionError==nil) {
-                                   //沒有資料，連線沒有錯誤
-                               } else if (connectionError != nil) {
-                                   //連線有錯誤
-                                   NSLog(@"error %@",connectionError);
-                               }
-                           }];
+    MKLocalSearch *search = [[MKLocalSearch alloc] initWithRequest:searchRequest];
+    [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+        if (response.mapItems.count > 0) {
+            places = [response.mapItems copy];
+            [locationView.searchedLocationTable reloadData];
+            locationView.searchedLocationTable.hidden = NO;
+
+        } else {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Oops!"
+                                                            message:@"No search results found! Try again with a different query."
+                                                           delegate:nil
+                                                  cancelButtonTitle:nil
+                                                  otherButtonTitles:@"OK", nil];
+            [alert show];
+        }
+    }];
 }
 
 - (void)saveLocation
@@ -153,10 +142,12 @@
     }
     
     // Configure the cell...
-    NSString *locName = [places[indexPath.row] objectForKey:@"name"];
-    NSString *address = [places[indexPath.row] objectForKey:@"formatted_address"];
+    MKMapItem *mapItem = [places objectAtIndex:indexPath.row];
+    NSString *locName = [mapItem.placemark.addressDictionary objectForKey:@"Name"];
     cell.textLabel.text = locName;
-    cell.detailTextLabel.text = address;
+
+    //NSString *address = [places[indexPath.row] objectForKey:@"formatted_address"];
+    //cell.detailTextLabel.text = address;
     return cell;
 }
 
@@ -170,21 +161,9 @@
 {
     locationView.searchedLocationTable.hidden = YES;
     mapView_.hidden = NO;
-    NSDictionary *location = [[places[indexPath.row] objectForKey:@"geometry"] objectForKey:@"location"];
-    NSString *lat = [location objectForKey:@"lat"];
-    NSString *lng = [location objectForKey:@"lng"];
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:[lat doubleValue]
-                                                            longitude:[lng doubleValue]
-                                                                 zoom:15];
-
-    [mapView_ setCamera:camera];
-    // Creates a marker in the center of the map.
-    GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake([lat doubleValue], [lng doubleValue]);
-    marker.title = [places[indexPath.row] objectForKey:@"name"];
-    marker.snippet = [places[indexPath.row] objectForKey:@"formatted_address"];
-    marker.map = mapView_;
-    locationView.locationField.text = marker.title;
+    MKMapItem *item = [places objectAtIndex:indexPath.row];
+    [self calculateRouteToMapItem:item];
+    locationView.locationField.text = [item.placemark.addressDictionary objectForKey:@"Name"];
 }
 
 #pragma mark - UITextFieldDelegate
@@ -195,7 +174,7 @@
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    [self queryGooglePlaces:locationView.locationField.text];
+    [self startSearchForText:locationView.locationField.text];
     return YES;
 }
 
@@ -208,5 +187,160 @@
     return screenShotImage;
 }
 
+- (MKMapItem*)mapItemForCoordinate:(CLLocationCoordinate2D)coordinate {
+    MKPlacemark *placemark = [[MKPlacemark alloc] initWithCoordinate:coordinate addressDictionary:nil];
+    MKMapItem *mapItem = [[MKMapItem alloc] initWithPlacemark:placemark];
+    return mapItem;
+}
+
+- (void)calculateRouteToMapItem:(MKMapItem*)item {
+    // 1
+    [self performAfterFindingLocation:^(CLLocationCoordinate2D userLocation) {
+        // 2
+        MKPointAnnotation *sourceAnnotation = [MKPointAnnotation new];
+        sourceAnnotation.coordinate = userLocation;
+        sourceAnnotation.title = @"Start";
+        
+        MKPointAnnotation *destinationAnnotation = [MKPointAnnotation new];
+        destinationAnnotation.coordinate = item.placemark.coordinate;
+        destinationAnnotation.title = @"End";
+        
+        __block MKRoute *fromUserLocationDirectionsRoute = nil;
+
+        // 1
+        MKMapItem *sourceMapItem = [self mapItemForCoordinate:userLocation];
+        MKMapItem *destinationMapItem = item;
+        
+        // 3
+        dispatch_group_t group = dispatch_group_create();
+        
+        // 4
+        // Find route to source airport
+        dispatch_group_enter(group);
+        [self obtainDirectionsFrom:sourceMapItem
+                                to:destinationMapItem
+                        completion:^(MKRoute *route, NSError *error) {
+                            fromUserLocationDirectionsRoute = route;
+                            dispatch_group_leave(group);
+                        }];
+        
+        // 6
+        // When both are found, setup new route
+        dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+            
+                [mapView_ addAnnotations:@[sourceAnnotation, destinationAnnotation]];
+            
+                [mapView_ addOverlay:fromUserLocationDirectionsRoute.polyline level:MKOverlayLevelAboveRoads];
+            
+                MKMapPoint points[2];
+                points[0] = MKMapPointForCoordinate(sourceAnnotation.coordinate);
+                points[1] = MKMapPointForCoordinate(destinationAnnotation.coordinate);
+                
+                MKCoordinateRegion boundingRegion = CoordinateRegionBoundingMapPoints(points, 2);
+                boundingRegion.span.latitudeDelta *= 1.1f;
+                boundingRegion.span.longitudeDelta *= 1.1f;
+                [mapView_ setRegion:boundingRegion animated:YES];
+        });
+    }];
+}
+
+- (void)obtainDirectionsFrom:(MKMapItem*)from to:(MKMapItem*)to completion:(void(^)(MKRoute *route, NSError *error))completion {
+    // 1
+    MKDirectionsRequest *request = [[MKDirectionsRequest alloc] init];
+    
+    // 2
+    request.source = from;
+    request.destination = to;
+    
+    // 3
+    request.transportType = MKDirectionsTransportTypeAutomobile;
+    
+    // 4
+    MKDirections *directions = [[MKDirections alloc] initWithRequest:request];
+    [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+        MKRoute *route = nil;
+        
+        // 5
+        if (response.routes.count > 0) {
+            route = response.routes[0];
+        } else if (!error) {
+            error = [NSError errorWithDomain:@"com.razeware.FlyMeThere" code:404 userInfo:@{NSLocalizedDescriptionKey:@"No routes found!"}];
+        }
+        
+        // 6
+        if (completion) {
+            completion(route, error);
+        }
+    }];
+}
+
+- (void)performAfterFindingLocation:(LocationCallback)callback {
+    if (mapView_.userLocation != nil) {
+        if (callback) {
+            callback(mapView_.userLocation.coordinate);
+        }
+    } else {
+        _foundLocationCallback = [callback copy];
+    }
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
+    if (_foundLocationCallback) {
+        _foundLocationCallback(userLocation.coordinate);
+        _foundLocationCallback = nil;
+    }
+}
+
+#pragma mark - MKMapViewDelegate
+
+- (MKAnnotationView*)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    if ([annotation isKindOfClass:[MKPlacemark class]]) {
+        MKPinAnnotationView *pin = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"placemark"];
+        if (!pin) {
+            pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"placemark"];
+            pin.pinColor = MKPinAnnotationColorRed;
+            pin.canShowCallout = YES;
+        } else {
+            pin.annotation = annotation;
+        }
+        return pin;
+    }
+    return nil;
+}
+
+- (MKOverlayRenderer*)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
+    NSLog(@"Renderer %@",overlay);
+
+    if ([overlay isKindOfClass:[MKPolyline class]]) {
+        MKPolylineRenderer *renderer = [[MKPolylineRenderer alloc] initWithPolyline:(MKPolyline*)overlay];
+        renderer.strokeColor = [UIColor blueColor];
+        return renderer;
+    }
+    return nil;
+}
+
+
+MKCoordinateRegion CoordinateRegionBoundingMapPoints(MKMapPoint *points, NSUInteger count) {
+    if (count == 0) {
+        return MKCoordinateRegionForMapRect(MKMapRectWorld);
+    }
+    
+    MKMapRect boundingMapRect;
+    boundingMapRect.origin = points[0];
+    boundingMapRect.size = MKMapSizeMake(0.0, 0.0);
+    
+    for (NSUInteger i = 1; i < count; i++) {
+        MKMapPoint point = points[i];
+        if (!MKMapRectContainsPoint(boundingMapRect, point)) {
+            boundingMapRect = MKMapRectUnion(boundingMapRect, (MKMapRect){.origin=point,.size={0.0,0.0}});
+        }
+    }
+    
+    MKCoordinateRegion region = MKCoordinateRegionForMapRect(boundingMapRect);
+    region.span.latitudeDelta = MAX(region.span.latitudeDelta, 0.001);
+    region.span.longitudeDelta = MAX(region.span.longitudeDelta, 0.001);
+    
+    return region;
+}
 
 @end
